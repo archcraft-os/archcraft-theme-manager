2b5625ad66606c1c5f117abd6f139aa86064382e
T-Dynamos <anshdadwal298@gmail.com>
2023-01-06 00:06:35 +0530

    some more functionality

--- main.py
+++ main.py
@@ -35,10 +35,14 @@ class ThemeManager(MDApp):
 
     def build(self):
         self.name_linux = os.popen("whoami").read()[:-1]
-        self.current_theme_file = "/home/{}/.config/openbox-themes/themes/.current".format(self.name_linux)
+        self.openbox_theme_dir = "/home/{}/.config/openbox-themes/themes/".format(self.name_linux)
+        self.openbox_theme_file = "/home/{}/.config/openbox-themes/themes/.current".format(self.name_linux)
+        self.bspwm_theme_dir = "/home/{}/.config/bspwm-themes/themes/".format(self.name_linux)
+        self.bspwm_theme_file = "/home/{}/.config/bspwm-themes/themes/.current".format(self.name_linux)
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.material_style = "M3"
         self.MainUI = Builder.load_file("main.kv")
+        self.InstallView = Builder.load_file("modal_views/install_theme.kv")
         from kivy.core.window import Window
         Window.size = [400,650]
         return self.MainUI
@@ -64,61 +68,72 @@ class ThemeManager(MDApp):
 
     def load_local_themes(self,*args):
         Animation(opacity=0,d=0.2).start(self.root.ids.local_themes)
-        Clock.schedule_once(self.add_local_theme_widget,0.3)
+        Clock.schedule_once(self.add_openbox_local_theme_widget,0.3)
         Clock.schedule_once(lambda arg : Animation(opacity=1,d=0.2).start(self.root.ids.local_themes),0.4)
 
-    def add_local_theme_widget(self,arg):
+    def add_openbox_local_theme_widget(self,arg):
         self.root.ids.local_themes.clear_widgets()
-        all_themes = self.get_all_themes()
-        current_theme = self.get_current_theme()
+        all_themes = self.get_all_openbox_themes()
+        current_theme = self.get_current_openbox_theme()
         all_themes.remove(current_theme)
         CurrentWidget = ThemeView()
         if os.path.isfile("./default_previews/{}.png".format(current_theme)):
             CurrentWidget.source = "./default_previews/{}.png".format(current_theme)
         else:
-            self.current_theme_file[:-9]+f"/{current_theme}/preview.png"
+            self.openbox_theme_dir+f"/{current_theme}/preview.png"
         CurrentWidget.text = current_theme.capitalize()
         CurrentWidget.children[0].style = "outlined"
         CurrentWidget.children[0].line_color = self.theme_cls.accent_light
         CurrentWidget.children[0].line_width = dp(2)
         CurrentWidget.ids.is_current.opacity = 1
         self.root.ids.local_themes.add_widget(CurrentWidget)
+        self.root.ids.openbox_scrollview.scroll_to(CurrentWidget)
 
         for theme in all_themes:
             TestWidget = ThemeView()
             if os.path.isfile("./default_previews/{}.png".format(theme)):
                 TestWidget.source = "./default_previews/{}.png".format(theme) 
             else:
-                TestWidget.source = self.current_theme_file[:-9]+f"/{theme}/preview.png"
+                TestWidget.source = self.openbox_theme_dir+f"/{theme}/preview.png"
             TestWidget.text = theme.capitalize()
             self.root.ids.local_themes.add_widget(TestWidget)
 
-    def apply_theme(self,theme):
-        if os.path.exists(self.current_theme_file[:-9]+f"/{theme}/apply.sh"):
-            _thread.start_new_thread(lambda x,y: os.system(which("bash")+" "+self.current_theme_file[:-8]+f"/{theme}/apply.sh &"),("",""))
+    def open_theme_installer(self,root):
+        self.InstallView.ids.theme_name.text = root.text.split(" by ")[0]
+        self.InstallView.ids.dev_name.text = root.text.split(" by ")[-1]
+        self.InstallView.open() 
+
+    def apply_theme_openbox(self,theme):
+        if os.path.exists(self.openbox_theme_file[:-9]+f"/{theme}/apply.sh"):
+            _thread.start_new_thread(lambda x,y: os.system(which("bash")+" "+self.openbox_theme_dir+f"/{theme}/apply.sh &"),("",""))
             Clock.schedule_once(self.load_local_themes)
 
-    def get_current_theme(self) -> str:
-        if os.path.isfile(self.current_theme_file):
-            with open(self.current_theme_file,"r") as file:
+    def get_current_openbox_theme(self) -> str:
+        if os.path.isfile(self.openbox_theme_file):
+            with open(self.openbox_theme_file,"r") as file:
                 self.current_theme = file.read().split("\n")[0]
                 file.close()
             return self.current_theme
         else:
             raise FileNotFoundError("It does'nt seems you have openbox-themes installed?")
 
-    def set_current_theme(self,theme:str) -> None:
-        if os.path.isfile(self.current_theme_file):
-            with open(self.current_theme_file,"w") as file:
+    def set_current_openbox_theme(self,theme:str) -> None:
+        if os.path.isfile(self.openbox_theme_file):
+            with open(self.openbox_theme_file,"w") as file:
                 file.write(theme)
                 file.close()
             return theme
         else:
             raise FileNotFoundError("It does'nt seems you have openbox-themes installed?")
 
-    def get_all_themes(self):
-        if os.path.isdir("/".join(self.current_theme_file.split("/")[:-1])):
-            return os.listdir("/".join(self.current_theme_file.split("/")[:-1]))[:-4]
+    def get_all_openbox_themes(self):
+        if os.path.isdir("/".join(self.openbox_theme_file.split("/")[:-1])):
+            files = os.listdir("/".join(self.openbox_theme_file.split("/")[:-1]))
+            folders = [] 
+            for file in files:
+                if os.path.isdir(self.openbox_theme_dir+file):
+                    folders.append(file)
+        return folders
 
 
 ThemeManager().run()
